91d61f8a98ac7b06793c49313395016e6b8f2996
AlexanderForster <62343048+AlexanderForster@users.noreply.github.com>
2020-03-19 13:47:13 +0100

    Update InsultGenerator.java
    0.0.1 - removing syntax error

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -8,7 +8,7 @@ public class InsultGenerator {
 	public String generateInsult() {
 		String vowels = "AEIOU";
 		String article = "an";
-		S1tring theInsult = "";
+		String theInsult = "";
 
 		try {
 			String databaseURL = "jdbc:postgresql://";
